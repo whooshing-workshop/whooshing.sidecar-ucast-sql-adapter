954138f45d7a29163417955727726ef67aeb62de
ChenLin Wang <wangchenlin2001@icloud.com>
2026-02-11 18:21:10 +0800

    V0.0.2.3 更新数据结构

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 console.log("Hello via Bun!");
+
+// 使用 prisma-case-format 工具
+// npx prisma-case-format --file ./prisma/schema.prisma --table-case pascal --field-case camel --map-table-case snake --map-field-case snake
